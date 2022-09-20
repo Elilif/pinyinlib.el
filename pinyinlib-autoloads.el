9eaef6822cc20144f3ad03f195723e487728e6d3
eli <qq1358722950@gmail.com>
2022-09-20 17:42:11 +0800

    feat: add quanpin

---
+++ pinyinlib-autoloads.el
@@ -0,0 +1,21 @@
+;;; pinyinlib-autoloads.el --- automatically extracted autoloads  -*- lexical-binding: t -*-
+;;
+;;; Code:
+
+
+;;;### (autoloads "actual autoloads are elsewhere" "pinyinlib" "pinyinlib.el"
+;;;;;;  (25149 29909 390527 515000))
+;;; Generated autoloads from pinyinlib.el
+
+(register-definition-prefixes "pinyinlib" '("pinyinlib-"))
+
+;;;***
+
+(provide 'pinyinlib-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; pinyinlib-autoloads.el ends here
